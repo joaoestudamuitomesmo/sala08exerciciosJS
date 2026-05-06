cb486351f41e0cfc938f3dbfce122441ffdb4a17
João Victor Marques da Silva <jmarquessva61@gmail.com>
2026-05-06 11:00:16 -0300

    Add files via upload

--- logicajs/print/script.js
+++ logicajs/print/script.js
@@ -5,5 +5,13 @@ function mostrar(){
 
     var corzinhapo = document.getElementById("i2").value
     var printcorzinhapo = document.getElementById('printcor')
-    printcorzinhapo.innerHTML = corzinhapo
+    printcorzinhapo.innerHTML = `A cor escolhida foi : ${corzinhapo}`
+
+    var datinhapo = document.getElementById("i3").value
+    var printdatinhapo = document.getElementById('printdata')
+    printdatinhapo.innerText = `A data escolhida foi : ${datinhapo}`
+
+    var checkboxzinhopo = document.getElementById("i4").checked
+    var printcheckboxzinhopo = document.getElementById('printcheckbox')
+    printcheckboxzinhopo.innerText = `A checkbox deu : ${checkboxzinhopo}`
 }
