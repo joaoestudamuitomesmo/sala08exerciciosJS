09f1e4e4f75410303b4acc210b2444792edfd103
João Victor Marques da Silva <jmarquessva61@gmail.com>
2026-05-07 07:57:47 -0300

    adwdadwadwad

---
+++ logicajs/portaria/script.js
@@ -0,0 +1,8 @@
+function check(){
+    var wasBorn = parseInt(document.getElementById('birth').value);
+    var res = document.getElementById('result')
+    var cliAge = document.getElementById('age')
+
+    res.textContent = "a"
+    cliAge.textContent ="b"
+}
